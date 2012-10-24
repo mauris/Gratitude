16f98b93b28b6bf19422b3fcae65d604c53f674f
Sam-Mauris Yong <mauris@hotmail.sg>
2012-10-24 16:17:12 +0800

    updated builder and fixed infinite loop

--- pack/src/ApiController.php
+++ pack/src/ApiController.php
@@ -6,22 +6,32 @@ class ApiController extends Controller {
     
     public function build(){
         // https://graph.facebook.com/300328703413848/likes
+        ignore_user_abort (false);
+        set_time_limit(0);
+        
         $url = 'https://graph.facebook.com/300328703413848/likes';
         $data = true;
-        $result = array();
+        //$result = array();
+        $fh = fopen('likes.json', 'w');
         while($url){
             $data = file_get_contents($url);
             if($data){
                 $data = json_decode($data, true);
                 if(isset($data['data'])){
-                    $result = array_merge($result, $data['data']);
+                    foreach($data['data'] as $like){
+                        fwrite($fh, $like['id'] . ' ' . $like['name'] . "\n");
+                    }
+                    //$result = array_merge($result, $data['data']);
                 }
                 if(isset($data['paging']['next'])){
                     $url = $data['paging']['next'];
+                }else{
+                    $url = null;
                 }
             }
         }
-        file_put_contents('likes.json', json_encode($result));
+        fclose($fh);
+        //file_put_contents('likes.json', json_encode($result));
     }
     
 }
