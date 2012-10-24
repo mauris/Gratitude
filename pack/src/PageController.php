ecaaa35995d5ff7596ccb46e1e9b8bed7085013b
Sam-Mauris Yong <mauris@hotmail.sg>
2012-10-24 18:24:41 +0800

    controller update

--- pack/src/PageController.php
+++ pack/src/PageController.php
@@ -5,7 +5,9 @@ use Packfire\Application\Pack\Controller;
 class PageController extends Controller{
     
     public function index(){
-        $this->state['data'] = json_decode(file_get_contents('likes.json'), false);
+        $cache = $this->service('cache');
+        /* @var $cache \Packfire\Cache\ICache */
+        $this->state['data'] = json_decode($cache->get('likes.json'), false);
         $this->render();
     }
     
