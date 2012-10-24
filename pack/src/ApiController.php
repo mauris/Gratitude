3b86fba44956bf7d5034eadce7ead6a74df4b7e4
Sam-Mauris Yong <mauris@hotmail.sg>
2012-10-24 18:12:38 +0800

    vote bar

--- pack/src/ApiController.php
+++ pack/src/ApiController.php
@@ -5,33 +5,38 @@ use Packfire\Application\Pack\Controller;
 class ApiController extends Controller {
     
     public function build(){
-        // https://graph.facebook.com/300328703413848/likes
-        ignore_user_abort (false);
-        set_time_limit(0);
-        
-        $url = 'https://graph.facebook.com/300328703413848/likes';
-        $data = true;
-        $result = array();
-        //$fh = fopen('likes.json', 'w');
-        while($url){
-            $data = file_get_contents($url);
-            if($data){
-                $data = json_decode($data, true);
-                if(isset($data['data'])){
-                    //foreach($data['data'] as $like){
-                    //    fwrite($fh, $like['id'] . ' ' . $like['name'] . "\n");
-                    //}
-                    $result = array_merge($result, $data['data']);
-                }
-                if(isset($data['paging']['next'])){
-                    $url = $data['paging']['next'];
-                }else{
-                    $url = null;
+        $cache = $this->service('cache');
+        /* @var $cache \Packfire\Cache\ICache */
+        if(!$cache->check('likes.json')){
+            // https://graph.facebook.com/300328703413848/likes
+            ignore_user_abort(true);
+            set_time_limit(0);
+
+            $url = 'https://graph.facebook.com/300328703413848/likes';
+            $data = true;
+            $result = array();
+            //$fh = fopen('likes.json', 'w');
+            while($url){
+                $data = file_get_contents($url);
+                if($data){
+                    $data = json_decode($data, true);
+                    if(isset($data['data'])){
+                        //foreach($data['data'] as $like){
+                        //    fwrite($fh, $like['id'] . ' ' . $like['name'] . "\n");
+                        //}
+                        $result = array_merge($result, $data['data']);
+                    }
+                    if(isset($data['paging']['next'])){
+                        $url = $data['paging']['next'];
+                    }else{
+                        $url = null;
+                    }
                 }
             }
+            //fclose($fh);
+            //file_put_contents('likes.json', json_encode($result));
+            $cache->set('likes.json', json_encode($result), 3600);
         }
-        //fclose($fh);
-        file_put_contents('likes.json', json_encode($result));
     }
     
 }
--- pack/src/PageIndexView.php
+++ pack/src/PageIndexView.php
@@ -5,6 +5,7 @@ use Packfire\Application\Pack\View;
 class PageIndexView extends View{
     
     function create(){
+        $this->define('counts', count($this->state['data']));
         $this->define('profiles', $this->state['data']);
     }
     
